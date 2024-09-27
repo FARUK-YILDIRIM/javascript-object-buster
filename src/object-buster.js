e0f730113b712a4dd222bb1202c02c3006d73d56
Faruk YILDIRIM <faruk.yildirim@outlook.com.tr>
2024-09-27 15:01:55 +0800

    feat: must

---
+++ src/object-buster.js
@@ -0,0 +1,33 @@
+class ObjectBuster {
+  _checkObject(obj) {
+    if (
+      typeof obj !== "object" ||
+      obj === null ||
+      Object.keys(obj).length === 0
+    ) {
+      throw new Error(`Expected a non-empty object`);
+    }
+  }
+
+  must(obj) {
+    this._checkObject(obj);
+    const keys = Object.keys(obj);
+    for (const key of keys) {
+      const value = obj[key];
+
+      if (value === undefined) {
+        throw new Error(`Property '${key}' cannot be undefined.`);
+      }
+
+      if (value === null) {
+        throw new Error(`Property '${key}' cannot be null.`);
+      }
+
+      if (typeof value === "object" && value !== null) {
+        this.must(value);
+      }
+    }
+  }
+}
+
+module.exports = new ObjectBuster();
